d6af6ee7b3b12560c9041d7a492b8e08710e1500
yangmingming <yangmingming@fang.com>
2021-03-17 12:23:26 +0800

    增加 自定义FIFO自旋锁；

--- noteT/src/main/java/com/java/note/Jdk/lock/FifoMutex.java
+++ noteT/src/main/java/com/java/note/Jdk/lock/FifoMutex.java
@@ -19,8 +19,8 @@ public class FifoMutex {
         boolean wasInterrupted = false;
         Thread current = Thread.currentThread();
         waiters.add(current);
-
         //只有队首线程可以获取锁
+        //是队首元素 直接进去挂起
         while (waiters.peek() == current || !locked.compareAndSet(false, true)) {
             LockSupport.park(this);
             if (Thread.interrupted()) {
@@ -33,6 +33,28 @@ public class FifoMutex {
         }
         System.out.println(Thread.currentThread().getName() + "\t" + "获取锁");
     }
+
+    public void lock2() {
+        boolean wasInterrupted = false;
+        Thread current = Thread.currentThread();
+        waiters.add(current);
+
+        // Block while not first in queue or cannot acquire lock
+        while (waiters.peek() != current || !locked.compareAndSet(false, true)) {
+            LockSupport.park(this);
+            if (Thread.interrupted()) // ignore interrupts while waiting
+            {
+                wasInterrupted = true;
+            }
+        }
+        waiters.remove();
+        if (wasInterrupted)          // reassert interrupt status on exit
+        {
+            current.interrupt();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t" + "获取锁");
+    }
+
 
     public void unlock() {
         locked.set(false);
@@ -47,6 +69,7 @@ public class FifoMutex {
             public void run() {
                 try {
                     mutex.lock();
+//                    mutex.lock2();
                     Thread.sleep(5000);
                     mutex.unlock();
                 } catch (Exception e) {
@@ -59,6 +82,7 @@ public class FifoMutex {
             public void run() {
                 try {
                     mutex.lock();
+//                    mutex.lock2();
                     mutex.unlock();
                 } catch (Exception e) {
                     e.printStackTrace();
