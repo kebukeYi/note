c0cba6b003295fbd252fe4d22c16fafdfafc2eac
yangmingming <yangmingming@fang.com>
2021-03-17 12:18:35 +0800

    增加 自定义FIFO自旋锁；

---
+++ noteT/src/main/java/com/java/note/Jdk/lock/FifoMutex.java
@@ -0,0 +1,70 @@
+package com.java.note.Jdk.lock;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * 先进先出的锁
+ *
+ * @version v1.0
+ */
+public class FifoMutex {
+
+    private final AtomicBoolean locked = new AtomicBoolean(false);
+    private final Queue<Thread> waiters = new ConcurrentLinkedQueue<Thread>();
+
+    public void lock() {
+        boolean wasInterrupted = false;
+        Thread current = Thread.currentThread();
+        waiters.add(current);
+
+        //只有队首线程可以获取锁
+        while (waiters.peek() == current || !locked.compareAndSet(false, true)) {
+            LockSupport.park(this);
+            if (Thread.interrupted()) {
+                wasInterrupted = true;
+            }
+            waiters.remove();
+            if (wasInterrupted) {
+                current.interrupt();
+            }
+        }
+        System.out.println(Thread.currentThread().getName() + "\t" + "获取锁");
+    }
+
+    public void unlock() {
+        locked.set(false);
+        LockSupport.unpark(waiters.peek());
+        System.out.println(Thread.currentThread().getName() + "\t" + "释放锁");
+    }
+
+    public static void main(String[] args) {
+        final FifoMutex mutex = new FifoMutex();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mutex.lock();
+                    Thread.sleep(5000);
+                    mutex.unlock();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "AA").start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    mutex.lock();
+                    mutex.unlock();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }, "BB").start();
+    }
+
+}
