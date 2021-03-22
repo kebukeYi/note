b7c06ca74462c01013ec9ad7b2a1ee498721ef8d
kebukeyi <193276660@qq.com>
2021-03-23 00:11:48 +0800

    增加 dubbo 的日志打印；

--- dubbo-provider/src/main/java/com/dubbo/provider/lmpl/ProviderApplication.java
+++ dubbo-provider/src/main/java/com/dubbo/provider/lmpl/ProviderApplication.java
@@ -11,7 +11,7 @@ public class ProviderApplication {
 
 
     public static void main(String[] args) throws Exception {
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext(new String[]{"classpath:dubbo-provider.xml"});
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring/dubbo-provider.xml");
         context.start();
         System.out.println("服务启动。。。");
         // 按任意键退出
--- webflux/src/main/java/com/mmy/webflux/webclient/GreetingWebClient.java
+++ webflux/src/main/java/com/mmy/webflux/webclient/GreetingWebClient.java
@@ -1,7 +1,7 @@
 package com.mmy.webflux.webclient;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.server.WebServer;
 import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
@@ -14,13 +14,12 @@ public class GreetingWebClient {
     @Value("${server.port}")
     public Integer port;
 
-
-
     private WebClient client = WebClient.create("http://localhost:" + Optional.ofNullable(port).orElse(8082));
 
     private Mono<ClientResponse> result = client.get().uri("/hello").accept(MediaType.TEXT_PLAIN).exchange();
 
     public String getResult() {
+        //用的是哪个线程 阻塞获取值的？
         //主动阻塞等待结果
         return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class)).block();
 //        return ">> result = " + result.flatMap(res -> res.bodyToMono(String.class));
